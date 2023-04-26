001737e5b1a8b5212127cef59e774b82a1cfa95c
ziraabubakarbello <ziraabubakarbello2gmail.com>
2023-04-26 08:39:06 -0700

    simple shell commit few codes

--- executeable.c
+++ executeable.c
@@ -12,37 +12,38 @@
 void execute(char **argv)
 {
 	int status;
-	pid_t child_pid;
+	pid_t my_pid;
 	char *shell_command = NULL;
 	char *executed_command = NULL;
 
-	child_pid = fork();
-	if (child_pid == 0)
-	{
-	if (argv)
-	{
-		/**getting the command*/
-		shell_command = argv[0];
+	my_pid = fork();
 
-		/**getting the path to the command*/
-		executed_command = getPath(shell_command);
+	if (my_pid < 0)
+	{
+		perror("fork error");
+	}
 
-		/**executing the command*/
-		if (execve(executed_command, argv, NULL) == -1)
+	if (my_pid == 0)
+	{
+		if (argv)
 		{
-			perror("Error!\n");
+			/**getting the command*/
+			shell_command = argv[0];
+
+			/**getting the path to the command*/
+			executed_command = getPath(shell_command);
+
+			/**executing the command*/
+			if (execve(executed_command, argv, NULL) == -1)
+			{
+				perror("Error!\n");
+			}
 		}
 	}
-	}
-	else if (child_pid > 0)
+	if (my_pid > 0)
 	{
-	do
-	{
-		waitpid(child_pid, &status, WUNTRACED);
-	}while(!WIFEXITED(status) && !WIFSIGNALED(status));
-	}
-	else
-	{
-		perror("Error\n");
+		do {
+			waitpid(my_pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
 	}
 }
